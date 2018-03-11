53feb351fc145879cc7a23506884fca3b77151af
Julian Hoang <project_b@julianhoang.com>
2018-03-11 13:29:50 -0400

    Add behaviour tree

---
+++ Assets/Scripts/BehaviourTree/BehaviourNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class BehaviourNode : ScriptableObject, IEnumerable<BehaviourStatus> {
+    public List<BehaviourNode> children;
+
+    public abstract IEnumerator<BehaviourStatus> GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/BehaviourTree/BehaviourTree.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTree : MonoBehaviour
+{
+    public BehaviourNode root;
+
+    void Start()
+    {
+        StartCoroutine(Execute());
+    }
+
+    IEnumerator Execute()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(ExecuteRoot());
+        }
+    }
+
+    IEnumerator ExecuteRoot()
+    {
+        foreach (var status in root)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
