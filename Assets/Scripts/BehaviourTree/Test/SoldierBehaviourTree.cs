34e78b919d9387c7e56fc95afb09210a5819968a
Julian Hoang <project_b@julianhoang.com>
2018-03-18 14:59:53 -0400

    Add behaviour tree
    Closes #1

---
+++ Assets/Scripts/BehaviourTree/Test/SoldierBehaviourTree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BehaviourTree))]
+public class SoldierBehaviourTree : MonoBehaviour {
+    public bool testCondition;
+    public BehaviourTree bt;
+
+	void Awake () {
+		bt = GetComponent<BehaviourTree>();
+        var root = ScriptableObject.CreateInstance<SequenceNode>();
+        var a = ScriptableObject.CreateInstance<PrintNode>();
+        a.message = "Node A";
+        var b = ScriptableObject.CreateInstance<PrintNode>();
+        b.message = "Node B";
+        root.children = new List<BehaviourNode>
+        {
+            a,
+            b
+        };
+        bt.root = root;
+    }
+
+}
