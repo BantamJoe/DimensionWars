dd6c3c268faa392a37e80f9ad614a44ba68a3ee0
Hoai An Luu <hoaia.luu@gmail.com>
2018-03-22 17:19:42 -0400

    Unit
    Unit, Squad, UnitMover, UnitHealth

---
+++ Assets/Scripts/Squad.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squad : MonoBehaviour
+{
+    GameObject selectedUnit;
+    bool isSelected;
+    bool isTarget;
+    Vector3 mousePos = new Vector3();
+    Unit waypoint;
+    List<Unit> squadUnits = new List<Unit>();
+    Unit unit;
+    
+
+    void Start()
+    {
+        var squadUnits = transform.GetComponentsInChildren<Unit>();
+        foreach(var squadUnit in squadUnits)
+        {
+            this.squadUnits.Add(squadUnit);
+        }
+        //waypoint = GameObject.FindObjectOfType<Unit>();
+        isSelected = false;
+        isTarget = false;
+
+        mousePos = new Vector3(Input.mousePosition.x, 0.0f, Input.mousePosition.z);
+
+    }
+
+    void Update()
+    {
+  
+
+    }
+
+    public void setSelected(GameObject selection)
+    {
+        
+        foreach (var squadUnit in squadUnits)
+        {
+            squadUnit.setSelected(selectedUnit); ;
+        }
+      
+    }
+
+    public void setUnselected()
+    {
+        foreach (var squadUnit in squadUnits)
+        {
+            squadUnit.setUnselected(); 
+        }
+       
+    }
+
+    public void setTarget(Unit target)
+    {
+       
+        foreach (var squadUnit in squadUnits)
+        {
+            squadUnit.setTarget(target);
+        };
+        
+    }
+
+    public void addWaypoint(Vector3 pos)
+    {
+        foreach (var squadUnit in squadUnits)
+        {
+            squadUnit.addWaypoint(pos);
+        } 
+    }
+}
---
+++ Assets/Scripts/UnitHealth.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour
+{
+    public int health;
+    private bool isDamaged;
+    private float timer = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = PlayerPrefs.GetInt("Health");
+
+        if (isDamaged)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            TimeEnd();
+        }
+    }
+
+    void takeDamage()
+    {
+        if (!isDamaged)
+        {
+            health--;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag ==  "Bullet")
+        {
+            takeDamage();
+            if (isDamaged)
+            {
+                Color color = gameObject.GetComponent<SpriteRenderer>().color;
+                color.a = 0.5f;
+                gameObject.GetComponent<SpriteRenderer>().color = color;
+            }
+        }
+    }
+
+    void TimeEnd()
+    {
+        Color color = gameObject.GetComponent<SpriteRenderer>().color;
+        color.a = 1.0f;
+        gameObject.GetComponent<SpriteRenderer>().color = color;
+        isDamaged = false;
+        timer = 2.0f;
+    }
+}
