1313b31f76c18f67934c5e3f1230975074612b72
Dylan Seto <dylan.seto@japache.ca>
2018-04-08 10:56:46 -0400

    adds tracer and muzzle effect to shooting.

--- Assets/Scripts/UnitAnimationController.cs
+++ Assets/Scripts/UnitAnimationController.cs
@@ -40,7 +40,54 @@ public class UnitAnimationController : MonoBehaviour
             yield break;
         }
         anim.SetBool("isFiring", true);
+
+		this.transform.GetChild(1) // hips
+			.GetChild(2) // spine
+			.GetChild(0) //spine 1
+			.GetChild(0) //spine 2
+			.GetChild(2) //right should
+			.GetChild(0) // right arm
+			.GetChild(0) // rightfore arm
+			.GetChild(0) //right hand
+			.GetChild(5) //riflen
+			.GetChild(2).gameObject.SetActive(true); // Muzzle effect
+
+		this.transform.GetChild(1) // hips
+			.GetChild(2) // spine
+			.GetChild(0) //spine 1
+			.GetChild(0) //spine 2
+			.GetChild(2) //right should
+			.GetChild(0) // right arm
+			.GetChild(0) // rightfore arm
+			.GetChild(0) //right hand
+			.GetChild(5) //riflen
+			.GetChild(3).gameObject.SetActive(true); // Tracer
+		
         yield return new WaitForSeconds(1);
+
         anim.SetBool("isFiring", false);
+
+		//Turn off particle f
+		this.transform.GetChild(1) // hips
+			.GetChild(2) // spine
+			.GetChild(0) //spine 1
+			.GetChild(0) //spine 2
+			.GetChild(2) //right should
+			.GetChild(0) // right arm
+			.GetChild(0) // rightfore arm
+			.GetChild(0) //right hand
+			.GetChild(5) //riflen
+			.GetChild(2).gameObject.SetActive(false); // Muzzle effect
+
+		this.transform.GetChild(1) // hips
+			.GetChild(2) // spine
+			.GetChild(0) //spine 1
+			.GetChild(0) //spine 2
+			.GetChild(2) //right should
+			.GetChild(0) // right arm
+			.GetChild(0) // rightfore arm
+			.GetChild(0) //right hand
+			.GetChild(5) //riflen
+			.GetChild(3).gameObject.SetActive(false); // Tracer
     }
 }
