043c627b029d25a02bfdc5f53e91f90f397af70b
Julian Hoang <project_b@julianhoang.com>
2018-04-10 00:07:01 -0400

    Chargers

---
+++ Assets/Scripts/BehaviourTree/ConcreteTree/StrategicAISpotterBehaviourTree.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BehaviourTree))]
+[RequireComponent(typeof(Unit))]
+public class StrategicAISpotterBehaviourTree : MonoBehaviour
+{
+    [SerializeField]
+    private BehaviourTree bt;
+    public Unit unit;
+
+    void Awake()
+    {
+        unit = GetComponent<Unit>();
+        bt = GetComponent<BehaviourTree>();
+
+        bt.context = new BehaviourNode.Context
+        {
+            unit = unit
+        };
+
+        bt.root = StrategicBehaviour();
+    }
+
+    BehaviourNode StrategicBehaviour()
+    {
+        var root = bt.CreateNode<SequenceNode>();
+        root.children = new List<BehaviourNode>
+        {
+            bt.CreateNode<SetTarget>(),
+            bt.CreateNode<OnlyOnce>(),
+            bt.CreateNode<Announce>().Initialize("Commander 2: There he is! Charge!"),
+        };
+        return root;
+    }
+}
