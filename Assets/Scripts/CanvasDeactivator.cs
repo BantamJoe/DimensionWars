51ac487c7fccf516d98132e129ddf484e2a9c95f
Philip Michael <philmichaelmusic@gmail.com>
2018-04-11 20:52:17 -0400

    Added functionality to player prefab to turn off the Canvas under the [CameraRig] if there is no right controller detected.

---
+++ Assets/Scripts/CanvasDeactivator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasDeactivator : MonoBehaviour
+{
+    #region ABOUT
+    /**
+     * Disables the Canvas it is on if the [CameraRig]'s features are not activated (not in VR mode).
+     * This can be done by checking if the controllers are active.
+     **/
+    #endregion
+
+    #region VARIABLES
+    [Tooltip("The right controller attached to the [CameraRig]. MUST BE SET.")]
+    public GameObject rightController;
+    #endregion
+
+    void Start()
+    {
+        if (!rightController)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if (rightController.activeSelf == false)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
