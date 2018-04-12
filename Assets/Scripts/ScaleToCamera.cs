ff95fefe7e0905e5ec1714da6476c0b0547582a2
Hoai An Luu <hoaia.luu@gmail.com>
2018-04-12 01:05:24 -0400

    Added position scaling
    Scales the position of the images relative to the distance from the camera

--- Assets/Scripts/ScaleToCamera.cs
+++ Assets/Scripts/ScaleToCamera.cs
@@ -8,12 +8,14 @@ public class ScaleToCamera : MonoBehaviour {
     public Camera cam;
     public float objectScale = 0.04f;
     private Vector3 initialScale;
+	private Vector3 initialPosition;
 
     // set the initial scale, and setup reference camera
     void Start()
     {
         // record initial scale, use this as a basis
         initialScale = transform.localScale;
+		initialPosition = transform.localPosition;
 
         // if no specific camera, grab the default camera
         if (cam == null)
@@ -26,5 +28,6 @@ public class ScaleToCamera : MonoBehaviour {
         Plane plane = new Plane(cam.transform.forward, cam.transform.position);
         float dist = plane.GetDistanceToPoint(transform.position);
         transform.localScale = initialScale * dist * objectScale;
+		transform.localPosition = initialPosition * dist * objectScale;
     }
 }
